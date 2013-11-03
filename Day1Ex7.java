f382c92c8e7970fc63a8867a5ccce22679f9314a
bbk-pij-2012-20 <sjianz01@mail>
2013-11-03 15:48:27 +0000

    The simple sorting 3 numbers from Day1

---
+++ Day1Ex7.java
@@ -0,0 +1,94 @@
+public class Day1Ex7 // Naive sorting
+{
+	public static void main(String[] args)
+	{
+		System.out.println("input 3 numbers");
+		int hi=Integer.parseInt(System.console().readLine());
+		int mid=Integer.parseInt(System.console().readLine());
+		int lo=Integer.parseInt(System.console().readLine());
+		int temp;
+/* How to understand how to arrive at the optimal 
+order of comparisons - 3 is the minimum: 
+  ___
+ |   |
+hi  mid lo
+      ___
+     |   |  
+hi  mid lo
+  _______
+ |       |
+hi  mid lo
+
+			hi | mid | lo
+if start:	3     4     5
+
+1) hi/mid:  4     3     5	(swapped)
+2) mid/lo:	4     5     3	(swapped)
+3) hi/lo:	4     5     3	(no change)
+4) hi/mid:  5	  4     3 --> NEEDED TO RE-COMPARE hi/mid
+
+---------------------------------------------------
+  ___
+ |   |
+hi  mid lo
+  _______
+ |       |
+hi  mid lo
+      ___
+     |   |  
+hi  mid lo
+
+			hi | mid | lo
+same start:	3     4     5
+
+1) hi/mid:  4     3     5	(swapped)
+2) hi/lo:	5     3     4	(swapped)
+3) mid/lo:	5     4     3  --> DONE IN 3 COMPARISONS
+---------------------------------------------------
+  _______
+ |       |
+hi  mid lo
+      ___
+     |   |
+hi  mid lo
+  ___
+ |   |  
+hi  mid lo
+
+			hi | mid | lo
+if start:	3     5     4
+
+1) hi/lo:   4     5     3 (swapped)
+2) mid/lo:	4     5     3 (no change)
+3) hi/mid:	5     4     3 --> DONE IN 3 COMPARISONS
+---------------------------------------------------
+
+So it looks like the only order that isn't optimal is one in which the 
+long the immediate neighbours (top/mid and mid/low) are compared before
+the distal values (top/low).
+
+*/
+
+		if(hi<mid) 
+		{
+			temp=mid;
+			mid=hi;
+			hi=temp;			
+		}
+
+		if(hi<lo)
+		{
+			temp=lo;
+			lo=hi;
+			hi=temp;			
+		}
+
+		if(mid<lo)
+		{
+			temp=lo;
+			lo=mid;
+			mid=temp;
+		}
+	System.out.printf("From hi to low: %d,%d,%d\n",hi,mid,lo);
+	}	
+}
