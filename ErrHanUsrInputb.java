41fd581d5b96179e860342a91d4a9996ce4ad52a
bbk-pij-2012-20 <sjianz01@mail>
2014-01-11 17:52:47 +0000

    Day15 Ex3b

--- ErrHanUsrInputb.java
+++ ErrHanUsrInputb.java
@@ -1,25 +1,23 @@
 //cd ~/Desktop/JavaCoding/JavaExercises
 /* Day 15 Ex3b*/
-//import java.lang.Exception;
-//import java.lang.NullPointerException;
 
 public class ErrHanUsrInputb {
+	private static int counter = -1; //added static member counter
 	public static void main(String[] args) {
 		int numbers = 0;
 		int input = 0;
-		int counter = 0;
-		
-		System.out.println("how many numbers you gonna input?");
+//		int counter = 0; //removed local counter
 		
 		do {
+			System.out.println("how many numbers you gonna input?");
 			numbers = parser_ExHandler(System.console().readLine());
 		} while (numbers == 0);
 
 		do {
-			System.out.println("enter number ("+numbers+" in total)");
-			input = parser_ExHandler(System.console().readLine());
-			counter++;
-		} while (counter<numbers);
+			System.out.println("enter input ("+numbers+" in total)");
+			input = parser_ExHandler(System.console().readLine());		
+//			counter++; // removed counter increment
+		} while (counter < numbers);
 	}
 	
 	public static int parser_ExHandler(String input) {
@@ -29,11 +27,13 @@ public class ErrHanUsrInputb {
 		} catch (NumberFormatException e) {
 			System.out.println("number format exception");
 			e.printStackTrace();
-//			continue;
+			counter--; // added decrement counter
+//			continue;  // removed continue
 		}	catch (Exception e) {
 			System.out.println("some exception other than NullPointerException");
 			e.printStackTrace();
 		}
+		counter++; //added increment counter
 		return n;
 	}
 }
