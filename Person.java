80b5ac6e8b8867c5c73883ad12a29fb7eb97174a
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-11-13 15:30:18 +0000

    AdultPerson.java

--- Person.java
+++ Person.java
@@ -1,17 +1,22 @@
-/**
-*	A person is defined by movement (as opposed to plants) * and by speech (as opposed to animals).
-*/
+//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises
+//	Day 8 exercises: introducing Interfaces and comments
 
-public interface Person 
-{ 
-	/**
+/**
+ *	A person is defined by movement (as opposed to plants) 
+ *	and by speech (as opposed to animals).
+ */
+
+public interface Person {
+ 
+   /**
 	*	Move a distance in a straight line, given in meters.
 	*/
-//	public void move(int distance);
+	public void move(int distance);
 	
-	/**
+   /**
 	*	Say something, printing it on screen.
 	*	It may or may not be a perfect transcription. 
 	*/
 	public void say(String message); 
+
 }
