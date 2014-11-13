bd91d43b4b010d693604aae12eff98aa922b4a9a
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-11-13 15:30:42 +0000

    moving to subfolders

--- AdultPerson.java
+++ AdultPerson.java
@@ -1,43 +1,58 @@
-public class AdultPerson implements Person 
-{ 
+//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises
+//	Day 8 exercises: introducing Interfaces and comments
+
+public class AdultPerson implements Person { 
+
 	private int situation;
 	private int energy;
 //	private Leg leftLeg;
 //	private Leg rightLeg;
 
-	/**
+   /**
 	*	Move a distance in a straight line, given in meters 
 	*/
-/*	public void move(int distance) 
-	{
-		if(rightLeg.isHealthy()&&leftLeg.isHealthy()) 
-		{
+
+/*	
+	public void move(int distance) {
+	
+		if (rightLeg.isHealthy() && leftLeg.isHealthy())  {
+		
 			run(distance);
-		} 
-		else
-		{
+		
+		} else {
+		
 			walk(distance);
+		
 		}
+	
 	}
 */
-	/**
+
+   /**
  	*	Say something
  	*/
-	public void say(String message) 
-	{
+	public void say(String message) {
+	
 		System.out.println(message);
+	
 	}
-	private void run(int distance) 
-	{ 
-		situation=situation+distance; 
+	
+	private void run(int distance) {
+	 
+		situation = situation + distance; 
 		energy--;
+	
 	}
-	private void walk(int distance) 
-	{
-		for(int i=0;i<distance;i++) 
-		{
+	
+	private void walk(int distance) {
+	
+		for (int i = 0; i < distance; i++) {
+		
 			situation++;
+		
 		} 
+	
 	}
-	// ...other methods, including constructors, come here... 
+	// ...other methods
+
 }
