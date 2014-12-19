71f375bd461d2109c85a94b5d3af4971d2c97a79
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-12-19 15:33:46 +0000

    Person moved

---
+++ Inheritance_AdhocPolymorphism/Person/KidPerson.java
@@ -0,0 +1,60 @@
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises/Inheritance/implements
+// Introducing Interfaces, their implementation and Javadoc comments : (Exercise in the notes on 'Interfaces')
+
+public class KidPerson implements Person {
+
+	private int position; 
+	private Brain brain;
+	
+	/**
+	*	Move a distance in a straight line, given in meters 
+	*/
+	public void move(int distance) {
+	
+		crawl(distance);
+	
+	}
+    
+    /**
+     *	Say something
+     */
+	public void say(String message) {
+	
+		String finalMsg = getUnderstoodWords(message);
+		System.out.println(finalMsg);
+	
+	}
+	
+	private void crawl(int distance) {
+	
+		for (int i = 0; i < distance; i++) {
+		
+                position++;
+                waitALittle();
+		
+		}
+	
+	}
+	
+	private String getUnderstoodWords(String text) {
+	 
+		String result = "";
+		String[] words = brain.divideIntoWords(text);
+		
+		for (int i = 0; i < words.length; i++) { 
+
+			if (brain.isKnown(words[i])) {
+			
+				result = result + words[i]; 
+				// if not, ignore it
+			
+			} 
+		
+		}
+		
+		return result;
+	
+	}
+	// ... other methods, including constructors, come here... 
+
+}
---
+++ Inheritance_AdhocPolymorphism/Person/SimpleClass.java
@@ -0,0 +1,18 @@
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises/Inheritance/implements
+// Introducing Interfaces, their implementation and Javadoc comments : (Exercise in the notes on 'Interfaces')
+
+public class SimpleClass {
+
+	public static void main(String args[]) {
+	
+		Person son = new AdultPerson(); 
+		
+		// move in front of mother 
+		// son.move(10);
+		
+		// give the message
+		son.say("I need some money for a new iPhone Mum.");
+	
+	}	
+
+}
