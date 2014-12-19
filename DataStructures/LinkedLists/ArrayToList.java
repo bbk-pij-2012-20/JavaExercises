d142f7a7d285bad6ddb8382ca5c60819877f7bdd
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-12-19 15:13:02 +0000

    moved ArrayToList

--- DataStructures/LinkedLists/ArrayToList.java
+++ DataStructures/LinkedLists/ArrayToList.java
@@ -1,51 +1,73 @@
-//cd desktop/JavaCoding/JavaExercises
+//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises/DataStructures/LinkedLists
+//Exercise 7.9 (Introduction to data structures & pointers)
+/*
+Create a static method that takes an array of integers and returns a linked list of 
+integers in the same order as the original array. You can put this method in a class 
+called ListUtilities so that you can easily use it for the following exercises.
 
-public class ArrayToList
-{
-        
-        public static void main(String args[]) throws Exception
-        {
-                int[] intArray={1,2,4,5,6,7,8,9,3,5,7,8,12,34,2,4,45,23};        
-                List list=ListUtilities.ArayToList(intArray);
-        }
+Initialising an array with 15-20 elements is very easy with curly-brackets-notation, 
+so it is common to create an array and then convert it into a dynamic structure 
+automatically.
+*/
+public class ArrayToList {
+
+	public static void main(String args[]) throws Exception {
+	
+		int[] intArray = {1, 2, 4, 5, 6, 7, 8, 9, 3, 5, 7, 8, 12, 34, 2, 4, 45, 23};        
+		List list = ListUtilities.ArayToList(intArray);
+    
+    }
+
 }
-class ListUtilities
-{
-        public static List ArayToList(int[] intAry)
-        {
-                List list=new List();
-                for(int i:intAry)
-                {
-                        System.out.println(i);
-                        list.insert(i);
-                }
-                return list;
-        }
+
+class ListUtilities {
+
+	public static List ArayToList(int[] intAry) {
+	
+		List list = new List();
+		
+		for (int i : intAry) {
+		
+			System.out.println(i);
+			list.insert(i);
+		
+		}
+		
+		return list;
+	}
+
 }
-class List
-{
-        private Node first=null;
+
+class List {
+
+	private Node first = null;
         
-        public void insert(int n)
-        {
-                Node nod=new Node(n);
-                nod.next=first;
+	public void insert(int n) {
+	
+		Node nod = new Node(n);
+		nod.next = first;
 //Why does 'nod.getNext()=first' cause compiler error 'unexpected type'?
-                first=nod;
-        }
+		first = nod;
+	}
+	
 }
-class Node
-{
-        private int n;
-        public Node next;
+
+class Node {
+
+	private int n;
+	public Node next;
         
-        public Node getNext()//I'd written this to accomodate a private visibility on Node next
-        {
-                return next;
-        }
-        public Node(int n)
-        {
-                this.n=n;
-                next=null;
-        }
+	public Node getNext() {
+		
+		return next;
+	
+	}
+	
+	public Node(int n) {
+	
+		this.n = n;
+		next = null;
+	
+	}
+
 }
