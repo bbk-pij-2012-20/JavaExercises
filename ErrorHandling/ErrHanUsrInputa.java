91bc0c4d7acc546adc6f522d989a9457bcb5969e
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-12-19 17:44:29 +0000

    moved into ErrorHandling subdirectory

--- ErrorHandling/ErrHanUsrInputa.java
+++ ErrorHandling/ErrHanUsrInputa.java
@@ -1,28 +1,43 @@
-//cd ~/Desktop/JavaCoding/JavaExercises
-/* Day 15 Ex3a */
-//import java.lang.Exception;
-//import java.lang.NullPointerException;
-
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises/ErrorHandling
+// Exercise 15.3a: Introduction to Error handling
+/*
+Write a program that reads 10 numbers from the user and then prints the mean average. 
+If the user inputs something that is not a number, the program should complain and ask 
+for a number again until 10 numbers have been introduced.
+*/
 public class ErrHanUsrInputa {
+
 	public static void main(String[] args) {
+
 		String str = "";
 		int counter = 0;
 		int[] numbers = new int[10];
 		
 		do {
+			
 			System.out.println("enter number (10 in total)");
 			str = System.console().readLine();
+		
 			try {
+		
 				int n = Integer.parseInt(str);//"line 16"
+		
 			} catch (NumberFormatException e) {
+		
 				System.out.println("number format exception at line 16");
 				e.printStackTrace();
 				continue;
+		
 			}	catch (Exception e) {
+		
 				System.out.println("some exception other than NullPointerException");
 				e.printStackTrace();
+		
 			}
+		
 			counter++;
-		} while (counter<10);
+	
+		} while (counter < 10);
+	
 	}
 }
--- ErrorHandling/ErrHanUsrInputb.java
+++ ErrorHandling/ErrHanUsrInputb.java
@@ -1,39 +1,62 @@
-//cd ~/Desktop/JavaCoding/JavaExercises
-/* Day 15 Ex3b*/
-
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises/ErrorHandling
+// Exercise 15.3b: Introduction to Error handling
+/* 
+Modify the program so that it first asks how many numbers the user wants to enter, and 
+then asks for the numbers. The computer should complain if the user enters something 
+that is not a number in both cases. Use methods to prevent code repetitions.
+*/
 public class ErrHanUsrInputb {
+
 	private static int counter = -1; //added static member counter
+
 	public static void main(String[] args) {
+
 		int numbers = 0;
 		int input = 0;
 //		int counter = 0; //removed local counter
 		
 		do {
+			
 			System.out.println("how many numbers you gonna input?");
 			numbers = parser_ExHandler(System.console().readLine());
+		
 		} while (numbers == 0);
 
 		do {
-			System.out.println("enter input ("+numbers+" in total)");
+	
+			System.out.println("enter input (" + numbers + " in total)");
 			input = parser_ExHandler(System.console().readLine());		
 //			counter++; // removed counter increment
+	
 		} while (counter < numbers);
+
 	}
 	
 	public static int parser_ExHandler(String input) {
+	
 		int n = 0;
+		
 		try {
+		
 			n = Integer.parseInt(input);
+		
 		} catch (NumberFormatException e) {
+		
 			System.out.println("number format exception");
 			e.printStackTrace();
 			counter--; // added decrement counter
 //			continue;  // removed continue
+	
 		}	catch (Exception e) {
+			
 			System.out.println("some exception other than NullPointerException");
 			e.printStackTrace();
+	
 		}
+	
 		counter++; //added increment counter
 		return n;
+	
 	}
+
 }
