108f3785da604bc3d13cb84537a0060eb9b918eb
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-12-19 18:09:51 +0000

    move

--- ErrorHandling/MorePatients.java
+++ ErrorHandling/MorePatients.java
@@ -1,20 +1,31 @@
-// ~/Desktop/JavaCoding/JavaExercises
-/*Day15 Ex4*/
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises/ErrorHandling
+// Exercise 15.4: Introduction to Error handling
+/* 
+Write a class that asks for data (name and year of birth) about new patients in a 
+hospital and keeps them in a list of Patient. The constructor of Patient must throw 
+an IllegalArgumentException if the age of the patient is negative or greater than 130.
+*/
+
 public class MorePatients {
+
 	public static void main(String[] args) {
+
 		String name = "";
 		String dob = "";
+
 		do {
+
 			System.out.println("input name and DOB of new patients,-1 to end");
 			System.out.println("name: ");
 			name = System.console().readLine();
 			System.out.println("DOB (yyyy): ");
 			dob = System.console().readLine();
 			new PatientList.Patient(name, dob);
-		}
-		while (!name.equals("-1")); 
-//not sure why entering -1 does not terminate the do{} part immediately..?
+
+		} while (!name.equals("-1")); 
+	
 	}
+
 }
 
 class PatientList {
@@ -22,29 +33,46 @@ class PatientList {
 	//other code for a list of some form
 
 	static class Patient {
+	
 		String name = "";
 		String dob = "";
 		int age = 0;
 		
 		public Patient(String name, String dob) {
+		
 			setName(name);
 			setAge(dob);
+	
 		}
+	
 		public void setName(String name) {
+	
 			this.name = name;
+	
 		}
+	
 		public void setAge(String dob) {
+	
 			int age = 2014 - Integer.parseInt(dob);
+
 			try {
+				
 				if (age < 0 || age > 130) {
+				
 					throw new IllegalArgumentException();
+			
 				}
+				
 				this.age = age;
+			
 			} catch (IllegalArgumentException e) {
+		
 				System.out.println("cant be less than 0 or more than 130 years old");
 				e.printStackTrace();
+	
 			}
+
 		}
+		
 	}
 }
-
--- ErrorHandling/PrimeDivisorList.java
+++ ErrorHandling/PrimeDivisorList.java
@@ -1,37 +1,65 @@
-//cd ~/Desktop/JavaCoding/JavaExercises
-/*Day15 Ex5
-Only done the exception part, not done the interface,TDD, or the 
-toString override ! */
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises/ErrorHandling
+// Exercise 15.5: Introduction to Error handling
+/*
+Create a class PrimeDivisorList. Integers (as in class Integer) can be added to/removed 
+from the list. If a null number is passed to the add(Integer) method, a 
+NullPointerException must be thrown. 
+If a non-prime number is added, an IllegalArgumentException must be thrown.
+Override the method toString() so that it returns something like: [ 2 * 3^2 * 7 = 126 ]
+for a list containing one 2, two 3, and one 7.
+Use the TDD methodology to create the class (interface,tests,implementation). 
+You can base your class on classes and interfaces from the Java Collections Library.
+*/
+// Only done the exception part, not done the interface,TDD, or the toString override!
 import java.lang.NullPointerException;
 import java.lang.IllegalArgumentException;
 
 public class PrimeDivisorList {
+	
 	private Integer firstInt = null;
 	public static void add(Integer integer) {
+	
 		if (integer == null) {
+	
 			throw new NullPointerException();
+	
 		}
+	
 		if (!isPrime(integer)) {
+	
 			throw new IllegalArgumentException();
+	
 		}
 //		new PrimeDivisorList(integer);
 	}
+	
 	private static boolean isPrime(int integer) { 
+	
 		boolean prime = true;
 		int n = integer - 1;
+		
 		while (n > 1 && prime) {
+		
 			if (integer % n == 0) {
+		
 				prime = false;
+		
 			} else {
+		
 				n--;
+		
 			}
+		
 		}
+		
 		return prime;
+	
 	}
 	
 	public static String toString() {
 		
 		return 
+	
 	}
 
 	public static void main(String[] args) {
@@ -43,28 +71,42 @@ public class PrimeDivisorList {
 			add(n4);
 			add(null);
 */		try { 		
+	
 			add(3);
 			add(7);
 			add(16);
+	
 		} catch (NullPointerException e) {
+	
 			e.printStackTrace();
 			System.out.println("that is SO null");
+	
 		} catch (IllegalArgumentException e) {
+	
 			e.printStackTrace();
 			System.out.println("caught you trying to pass a non-prime");
+	
 		}
+	
 		try { 
+	
 			Integer n4 = null;
 			add(n4);
 			add(null);
+	
 		} catch (NullPointerException e) {
+	
 			e.printStackTrace();
 			System.out.println("that is SO null");
+	
 		} catch (IllegalArgumentException e) {
+	
 			e.printStackTrace();
 			System.out.println("caught you trying to pass a non-prime");
+	
 		}
 		
 //		System.out.println("calculation using the integers: "+new PrimeDivisorList().toString());
-	}	
+	}
+		
 }
