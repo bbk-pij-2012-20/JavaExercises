aaaf1abd696c5378a09e741abadc49b853ab71c5
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-12-23 13:59:42 +0000

    Result enum

---
+++ 05_ARRAYS/Result.java
@@ -0,0 +1,21 @@
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/05_ARRAYS
+// Exercise 6.6 (Arrays. 3D Array and also first use of enums as well).
+/*
+Create an enumerated type Result in its own file. The enum must have 8 possible values: HIT, FAIL LEFT,
+FAIL RIGHT, FAIL HIGH, FAIL LOW, FAIL SHORT, FAIL LONG, OUT OF RANGE. Hint: the enum must be
+public.
+Then create a Java class Target with the following methods:
+*/
+
+public enum Result {
+
+	HIT, 
+	FAIL LEFT, 
+	FAIL RIGHT,
+	FAIL HIGH,
+	FAIL LOW,
+	FAIL SHORT,
+	FAIL LONG,
+	OUT OF RANGE
+
+}
