e50bf93b522854037b05c34d3666310812286254
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-12-30 18:34:25 +0000

    starting the algorithms

---
+++ 06_DATA_STRUCTURES/LinkedLists/SortingAlgorithms/CocktailSort.java
@@ -0,0 +1,20 @@
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/06_DATA_STRUCTURES/LinkedLists/SortingAlgorithms
+// Exercise 7.12 (Using data structures & pointers to implement Sorting Algorithms)
+/*
+Cocktail sort is a combination of two bubble sorts. First the list is traversed in one direction to move the highest
+element to the end, and then it is traversed in the opposite direction to move the lowest element to the beginning.
+This bidirectional process is repeated until there are no swaps in the list, which shows that the list is sorted.
+Implement cocktail sort in a static method in a class, maybe ListUtilities.cocktailSort(List). The name
+of the algorithm comes from a metaphorical cocktail shaker moving up and down, up and down. . .
+Try your method with the same lists as bubble sort and see how long it takes to sort them.
+*/
+
+public class CocktailSort {
+
+	public static void main(String[] args) {
+	
+	
+	
+	}
+
+}
---
+++ 06_DATA_STRUCTURES/LinkedLists/SortingAlgorithms/QuickSort.java
@@ -0,0 +1,18 @@
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/06_DATA_STRUCTURES/LinkedLists/SortingAlgorithms
+// Exercise 7.13 (Using data structures & pointers to implement Sorting Algorithms). Difficulty level: very.
+/*
+This is a popular sorting algorithm that is very fast in most situations. Once you have finished all the other exercises,
+look up in wikipedia how this method works and make your own implementation (put it in ListUtilities.quickSort(List)).
+Then compare its performance with the other two methods. In order to see significant differences, you will need to
+use lists of some length.
+*/
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+	
+	
+	
+	}
+
+}
