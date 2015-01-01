b717bcba81773a9a326cfe54dd647447e4118d7a
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-01-01 02:47:39 +0000

    1st attempt at Bubble Sort

--- 06_DATA_STRUCTURES/LinkedLists/SortingAlgorithms/BubbleSort.java
+++ 06_DATA_STRUCTURES/LinkedLists/SortingAlgorithms/BubbleSort.java
@@ -15,12 +15,184 @@ Try your method with different lists and see how long it takes to sort them. You
 System.currentTimeMillis() to print on the screen the current time (measured in milliseconds since 1st January
 1970).
 */
-
+// so I'll make an unsorted singly linked list first, then sort it.
 public class BubbleSort {
 
+	private Node first = null;
+	
 	public static void main(String[] args) {
 	
+		BubbleSort bs = new BubbleSort();
+		bs.makeSLL();
+		bs.sort();
+		System.out.println("\nafter sorting list");
+		bs.print();
+		
+	}
 	
+	public void makeSLL() {
+	
+		Node node0 = new Node(10);
+		Node node1 = new Node(1);
+		Node node2 = new Node(25);
+		Node node3 = new Node(12);		
+		Node node4 = new Node(248);
+		Node node5 = new Node(14);
+		Node node6 = new Node(125);
+		Node node7 = new Node(642);		
+		Node node8 = new Node(38);
+		Node node9 = new Node(67);
+		Node node10 = new Node(19);
+		System.out.println("at start");
+		print();
+		add(node0);
+		add(node1);
+		add(node2);
+		add(node3);
+		add(node4);
+		add(node5);
+		add(node6);
+		add(node7);
+		add(node8);
+		add(node9);
+		add(node10);
+		System.out.println("after making list");
+		print();
+				
+	}
+	
+	public void add(Node node) {
+	
+		if (first == null) {
+		
+			first = node;
+		
+		} else {
+		
+			first.add(node);
+		
+		}
+		
+	}
+	
+	public void sort() {
+
+		Node temp = first;
+		boolean sorted = false;
+
+		if (first.getN() > first.getNext().getN()) {
+	
+			first = first.getNext();
+			temp.setNext(first.getNext());			
+			first.setNext(temp);
+
+		}
+
+		while (!sorted) {
+			
+			sorted = first.sort(true);
+
+		}
+
+	}
+	
+	private void print() {
+		
+		if (first == null) {
+		
+			System.out.println("empty list");
+		
+		} else {
+		
+			first.print();
+		
+		}
+	
+	}
+	
+}
+
+class Node {
+
+	private Node next = null;
+	private int n = 0;
+	
+	public Node(int n) {
+	
+		this.n = n;
+		next = null;
+	
+	}
+
+	public int getN() {
+	
+		return n;
+	
+	}
+	
+	public Node getNext() {
+	
+		return next;
+	
+	}
+	
+	public void setNext(Node node) {
+	
+		next = node;
+	
+	}
+	
+	public void add(Node node) {
+	
+		if (next == null) {
+		
+			next = node;
+		
+		} else {
+		
+			next.add(node);
+		
+		}
+	
+	}
+	
+	public boolean sort(boolean unchanged) {
+
+		boolean unchanged2 = unchanged;
+		
+		if (next == null) {
+	
+			return unchanged;
+		
+		} else {
+			
+			if (next.next != null && next.n > next.next.n) {
+		
+				Node temp = next;
+				next = next.next;
+				temp.next = next.next;
+				next.next = temp;
+				unchanged2 = false;
+				
+			}
+
+			return unchanged && next.sort(unchanged2);
+
+		}
+		
+	}
+	
+	
+		
+	public void print() {
+	
+		System.out.println("node: " + n);
+	
+		if (next != null) {
+		
+			next.print();
+		
+		}	
 	
 	}
 
