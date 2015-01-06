d4be362335674c0b55e3856d137b728485147cb5
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-01-06 14:35:00 +0000

    changes not staged for commit

--- 06_DATA_STRUCTURES/LinkedLists/CircularlyLinkedList/CircularLinkList.java
+++ 06_DATA_STRUCTURES/LinkedLists/CircularlyLinkedList/CircularLinkList.java
@@ -1,5 +1,17 @@
-//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaCoding/JavaExercises/DataStructures/LinkedLists
-//Exercise 7.6 (Introduction to data structures & pointers)
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/06_DATA_STRUCTURES/LinkedLists/CircularlyLinkedList
+// Exercise 7.6 (Introduction to data structures & pointers)
+/*
+A circular list is a dynamic list in which there is a beginning but there is no end: the last element is pointing back
+to the first element. Circular lists can be singly- or doubly-linked. In this exercise, you must create a singly-linked
+circular list using the “hospital and patients” example as a starting point.
+1. Create the circular list and add several elements to it (around 10 is fine).
+2. Traverse it forwards for one complete loop. While you go around the list, print out the content of each element.
+How do you know that you have reached the end of the list when you never find a null pointer?
+3. Delete a couple of elements from the list.
+4. Traverse it again printing out the content of each element.
+5. Add a new element to the list. Try to delete an element that is NOT in the list.
+6. Traverse it again printing out the content of each element.
+*/
 
 public class CircularLinkList {
 
