3fd8511e710640242101313092bed18a94b1289c
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-01-31 22:39:48 +0000

    remove method has runtime problem

--- 05_DATA_STRUCTURES/TREES/IntegerTree.java
+++ 05_DATA_STRUCTURES/TREES/IntegerTree.java
@@ -17,19 +17,19 @@ Add a method toString() to the class. This methods must return a representation 
 
 9.1.3.
 Add a method depth() that returns the number of levels in a tree. By convention, a tree with only one node(i.e. the root) has a depth of zero. Hint: the depths of the trees in Figure 1 are 0, 1, 2, and 3.Hint: the depth of a tree is one more than the deepest of its subtreees
-*/
 
-/*
+9.1.4.
+Add a method remove(int) to the class. This method must look for the node that contains the given value and
+remove it from the tree.
+Hint: removing leafs is trivial; to remove nodes, you can replace the removed node with its highest element on
+its left or the lowest on its right.
 
-        5		0
-       1 42		1
-        6 57	2
-         8 58	3
-        69	4
-          68 99	5
-         67		6
-        66		7
-
+9.1.5.
+Trees work very well with unsorted data because they automatically sort it as they are filled up with data. There is
+a problem with already sorted data, though: all elements are placed on the same branches and the “natural sorting”
+effect is lost, the tree becomes just a list with an additional null pointer at every level.
+Add a method rebalance() to your tree that re-arranges the tree to make it balanced, i.e. having approximately
+the same number of nodes on both branches.
 
 */
 
@@ -60,6 +60,10 @@ public class IntegerTree {
     System.out.println("getMin() should give 1: " + tree.getMin());
     System.out.println("toString(): " + tree.toString());
     System.out.println("depth(): " + tree.depth());
+  //  tree.remove(5);
+  //  System.out.println("toString(): " + tree.toString());
+    tree.remove(58);
+    System.out.println("toString(): " + tree.toString());
 
   }
 
@@ -179,6 +183,120 @@ public class IntegerTree {
     }
 
     return root.getMin();
+
+  }
+
+  public boolean remove(int n) {
+
+    boolean nodeRemove = true;
+
+    if (root == null) {
+
+      nodeRemove = false;
+
+    } else if (contains(n)) {
+
+      nodeRemove = true;
+
+      if (root.n == n) {
+
+        reorderTreeAfterRemovingRoot();
+
+      } else {
+
+        root.remove(root, n);
+
+      }
+
+    }
+
+    return nodeRemove;
+
+  }
+
+  /**
+  * Reorders the tree when the node to remove is the root.
+  */
+  public void reorderTreeAfterRemovingRoot() {
+
+    IntegerTreeNode replacement = null;
+    IntegerTreeNode replacementParent = null;
+
+    if (root.left != null) {
+
+      replacementParent = retrieveParentOfReplacementFrom("left", root.left, root);
+
+      if (replacementParent == root) {
+
+        replacement = replacementParent.left;
+        replacement.right = root.right;
+
+      } else {
+
+        replacement = replacementParent.right;
+        replacement.right = root.right;
+        replacementParent.right = replacement.left;
+        replacement.left = root.left;
+
+      }
+
+    } else if (root.right != null) {
+
+      replacementParent = retrieveParentOfReplacementFrom("right", root.right, root);
+
+      if (replacementParent == root) {
+
+        replacement = replacementParent.right;
+        replacement.left = root.left;
+
+      } else {
+
+        replacement = replacementParent.left;
+        replacement.left = root.left;
+        replacementParent.left = replacement.right;
+        replacement.right = root.right;
+
+      }
+
+    }
+
+    root = replacement;
+
+  }
+
+  /**
+  * Retrieves the parent of the appropriate node according to the subtree specified.
+  * (The parent is required because the pointer to the child that will be relocated must itself be redirected).
+  * Hence, if the left subtree is specifed, node retrieved should be that with the highest int n.
+  * If the right subtree is specified, node retrieved should be that with the lowerst int n.
+  */
+  public IntegerTreeNode retrieveParentOfReplacementFrom(String subtree, IntegerTreeNode child, IntegerTreeNode parent) {
+
+    if (subtree.equals("left")) {
+
+      if (child.right == null) {
+
+        return parent;
+
+      } else {
+
+        return retrieveParentOfReplacementFrom("left", child.right, child);
+
+      }
+
+    } else {
+
+      if (child.left == null) {
+
+        return parent;
+
+      } else {
+
+        return retrieveParentOfReplacementFrom("right", child.left, child);
+
+      }
+
+    }
 
   }
 
@@ -388,6 +506,153 @@ public class IntegerTree {
       } else {
 
         return left.getMin();
+
+      }
+
+    }
+
+    /**
+    * Traverses the tree to find the node with int n, and removes it from the tree.
+    * The necessary subsequent re-ordering of the tree is encapsulated in separate
+    * functions.
+    */
+    private void remove(IntegerTreeNode node, int n) {
+
+      if (node.left != null) {
+
+        if (n < node.n) {
+
+          if (n == node.left.n) {
+
+            reorderTreeAfterRemoving(node.left, "left", node);
+
+          } else {
+
+            remove(node.left, n);
+
+          }
+
+        }
+
+      } else if (node.right != null) {
+
+        if (n > node.n) {
+
+          if (n == node.right.n) {
+
+            reorderTreeAfterRemoving(node.right, "right", node);
+
+          } else {
+
+            remove(node.right, n);
+
+          }
+
+        }
+
+      }
+
+    }
+
+    /**
+    * Receives both the node that is to be removed, as well as its parent node.
+    * These two should not be confused with the node that will replace the removed node,
+    * and its parent node.
+    */
+    private void reorderTreeAfterRemoving(IntegerTreeNode nodeToRemove, String childOf, IntegerTreeNode removeParent) {
+
+      IntegerTreeNode replacement = null;
+      IntegerTreeNode replacementParent = null;
+      IntegerTreeNode removeParent_ = removeParent;
+
+      if (nodeToRemove.left != null) {
+
+        replacementParent = retrieveParentOfReplacementFrom("left subtree of", nodeToRemove, removeParent);
+
+        if (replacementParent == nodeToRemove) {
+
+          replacement = nodeToRemove.left;
+          replacement.right = nodeToRemove.right;
+
+        } else {
+
+          replacement = replacementParent.right;
+          replacement.right = nodeToRemove.right;
+          replacementParent.right = replacement.left;
+          replacement.left = nodeToRemove.left;
+
+        }
+
+      } else if (nodeToRemove.right != null) {
+
+        replacementParent = retrieveParentOfReplacementFrom("right subtree of", nodeToRemove, removeParent);
+
+        if (replacementParent == nodeToRemove) {
+
+          replacement = nodeToRemove.right;
+          replacement.left = nodeToRemove.left;
+
+        } else {
+
+          replacement = replacementParent.left;
+          replacement.left = nodeToRemove.left;
+          replacementParent.left = replacement.right;
+          replacement.right = nodeToRemove.right;
+
+        }
+
+      }
+
+      if (childOf.equals("left")) {
+
+        removeParent_.left = replacement;
+
+      } else if (childOf.equals("right")) {
+
+        removeParent_.right = replacement;
+
+      }
+
+    }
+
+    /**
+    * Takes the node to be removed as the 'parent'. The first potential replacement node is
+    * the 'child' node (be it on left of right).
+    *
+    * Retrieves the parent of the node that will replace the node to be removed.
+    * (The parent is required because the pointer that the parent uses to point to the replacement
+    * node will have to be reset to one of the replacement node's child nodes (before the replacement
+    * node can be relocated).
+    *
+    * This would not be necessary if the tree was a threaded tree structures (i.e. where nodes can point back up the tree)).
+    * Hence, if the left subtree is specifed, node retrieved should be that with the highest int n.
+    * If the right subtree is specified, node retrieved should be that with the lowerst int n.
+    */
+    private IntegerTreeNode retrieveParentOfReplacementFrom(String subtree, IntegerTreeNode child, IntegerTreeNode parent) {
+
+      if (subtree.equals("left subtree of")) {
+
+        if (child.right == null) {
+
+          return parent;
+
+        } else {
+
+          return retrieveParentOfReplacementFrom("left subtree of", child.right, child);
+
+        }
+
+      } else {
+
+        if (child.left == null) {
+
+          return parent;
+
+        } else {
+
+          return retrieveParentOfReplacementFrom("right subtree of", child.left, child);
+
+        }
 
       }
 
