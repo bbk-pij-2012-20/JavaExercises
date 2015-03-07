a3e99e0e80e486cdc3135d0465a97a3c768cd726
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-03-07 15:51:49 +0000

    renamed and moved SimpleClass to PeoplePSVM

---
+++ 07_INHERITANCE_ADHOCPOLYMORPHISM/People/PeoplePSVM.java
@@ -0,0 +1,18 @@
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/People
+// Introducing Interfaces, their implementation and Javadoc comments : (Exercise in the notes on 'Interfaces')
+
+public class PeoplePSVM {
+
+	public static void main(String args[]) {
+
+		Person son = new AdultPerson();
+
+		// move in front of mother
+		// son.move(10);
+
+		// give the message
+		son.say("I need some money for a new iPhone Mum.");
+
+	}
+
+}
