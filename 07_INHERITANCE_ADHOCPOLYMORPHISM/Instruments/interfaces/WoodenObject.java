c6f0cb5e76259a24cac639fe4ab18f085ccbbbdb
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-03-07 17:18:40 +0000

    ex 10.6

---
+++ 07_INHERITANCE_ADHOCPOLYMORPHISM/Instruments/interfaces/WoodenObject.java
@@ -0,0 +1,11 @@
+//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/Instruments
+//Exercise 10.5: Introduction to inheritance (aka ad hoc polymorphism).
+
+public interface WoodenObject {
+
+  /**	@param no parameters
+	 *	@return void return type, just prints a message.
+	 */
+	public void burn();
+
+}
