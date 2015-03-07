8a1494e16a98da72757b63dbf15e748c6d84ce6a
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-03-07 15:35:17 +0000

    comment edit

--- 07_INHERITANCE_ADHOCPOLYMORPHISM/Animals/AnimalsPSVM.java
+++ 07_INHERITANCE_ADHOCPOLYMORPHISM/Animals/AnimalsPSVM.java
@@ -1,4 +1,4 @@
-//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/Animal
+//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/Animals
 //Exercise 10: Introduction to inheritance (aka ad hoc polymorphism).
 
 public class AnimalsPSVM {
