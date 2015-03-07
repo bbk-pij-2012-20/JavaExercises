442c5d3674a6908601f8d8dda593e63c5c3b1d24
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-03-07 15:35:48 +0000

    comment edit

--- 07_INHERITANCE_ADHOCPOLYMORPHISM/Animals/Dog.java
+++ 07_INHERITANCE_ADHOCPOLYMORPHISM/Animals/Dog.java
@@ -1,4 +1,4 @@
-//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/Animal
+//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/Animals
 //Exercise 10: Introduction to inheritance (aka ad hoc polymorphism).
 
 public class Dog extends Animal {
@@ -16,30 +16,3 @@ public class Dog extends Animal {
 		return "SAUSAGES !";
 
 	}
-
-abstract class Animal {
-
-	protected int oxygen = 0;
-	public abstract void move(int meters);
-	public abstract String makeSound();
-
-	public void breath(int air) {
-
-		this.oxygen += air / 4;
-
-	}
-
-}
-
-/* Animal starts out as an interface. Some implementation is entered into breath(),
-as such Animal becomes an abstract class and the Dog and Human classes that used to
-implement Animal, now extend it.
-
-public interface Animal {
-
-	void move(int meters);
-	String makeSound();
-	void breath(int air);
-
-}
-*/
