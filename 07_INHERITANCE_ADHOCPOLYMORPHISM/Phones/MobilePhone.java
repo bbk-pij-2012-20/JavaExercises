c94234f892954080c79c5fae4d22a99400cf3f41
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-03-08 14:51:09 +0000

    replaced last 10 nums array with deque arrayDeque. printLastNumbers() Works properly now

--- 07_INHERITANCE_ADHOCPOLYMORPHISM/Phones/MobilePhone.java
+++ 07_INHERITANCE_ADHOCPOLYMORPHISM/Phones/MobilePhone.java
@@ -1,21 +1,23 @@
-// cd
-// Exercise 10.1 continued... (Introducing ad hoc polymorphism (aka inheritance) and casting.)
+//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/Phones
+// Exercise 10.1 continued... Introducing ad hoc polymorphism (aka inheritance) and casting.
 
 /*
 '..Now create a class MobilePhone that extends OldPhone and adds methods for things like ringAlarm(String)
 and playGame(String). This class keeps a list of the last ten numbers that have been called, which can be printed
 with the method printLastNumbers()...''
 */
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
 
 public class MobilePhone extends OldPhone {
 
-  private String[] numList;
-  private int nextEmptyPosition = 0;
-  private boolean phoneBookFull = false;
+  private Deque<String> mostRecentlyCalledNumbersList;
+  private final int CALLS_STORE_LIMIT = 10;
 
   public MobilePhone() {
 
-    numList = new String[10];
+    mostRecentlyCalledNumbersList = new ArrayDeque<>(CALLS_STORE_LIMIT);
 
   }
 
@@ -34,45 +36,31 @@ public class MobilePhone extends OldPhone {
   @Override
   public void call(String number) {
 
-    if (!phoneBookFull) {
+    super.call(number);
 
-      numList[nextEmptyPosition++] = number;
+    if (mostRecentlyCalledNumbersList.size() >= CALLS_STORE_LIMIT) {
 
-      if (nextEmptyPosition == numList.length) {
-
-        phoneBookFull = true;
-        nextEmptyPosition--;
-
-      }
-
-    } else {
-
-      numList[nextEmptyPosition] = number;
+      mostRecentlyCalledNumbersList.removeFirst();
 
     }
+
+    mostRecentlyCalledNumbersList.addLast(number);
 
   }
 
   public void printLastNumbers() {
 
-    if (numList == null) {
+    if (mostRecentlyCalledNumbersList.isEmpty()) {
 
-      System.out.println("phone numbers list is empty");
+      System.out.println("nothing to print - you've not made a single phone call with this phone");
 
-    }
+    } else {
 
-    int i = 0;
-    boolean end = false;
+      System.out.println("most recently called numbers (..most recent first ..no more than last 10)");
 
-    while (!end && numList[i] != null && i < numList.length) {
+      for (Iterator it = mostRecentlyCalledNumbersList.descendingIterator(); it.hasNext();) {
 
-      int num = i + 1;
-      System.out.println("number " + num + ": " + numList[i]);
-      i++;
-
-      if (i == numList.length) {
-
-        end = true;
+        System.out.println(it.next());
 
       }
 
