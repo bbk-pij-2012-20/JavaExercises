008b68d9e170da71678924f9e69e8fc6d5215efe
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-03-09 18:44:41 +0000

    completed smartPhone

--- 07_INHERITANCE_ADHOCPOLYMORPHISM/Phones/RestrictedSmartPhone.java
+++ 07_INHERITANCE_ADHOCPOLYMORPHISM/Phones/RestrictedSmartPhone.java
@@ -6,6 +6,14 @@
 Create a class RestrictedSmartPhone that overrides playGame(String) to make it
 private and thus non-visible to external classes and scripts.
 Is this possible? Why?
+
+ANSWER:
+Not possible to restrict access to the method in this way because
+the visbility of the base class can not be greater than that in the derived class.
+I have however restricted access to mobile/smartPhone's playGame() method in
+this class simply by not calling the super class's method. Hence, overriding the
+method already effectively blocks the base class's method, so that for the base
+class's method to be called requires the super call.
 */
 
 public class RestrictedSmartPhone extends SmartPhone {
@@ -20,6 +28,11 @@ public class RestrictedSmartPhone extends SmartPhone {
 
   }
 
+  @Override
+  public void playGame(String game) {
 
+    System.out.println("access to playing games is restricted! You may not play " + game + " today.");
+
+  }
 
 }
