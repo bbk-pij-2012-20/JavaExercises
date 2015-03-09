893be4b45e8dcea151e9666f15cae7eb85faf59f
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-03-09 10:37:27 +0000

    ex 10.3 re:constructor chaining

--- 07_INHERITANCE_ADHOCPOLYMORPHISM/Phones/OldPhone.java
+++ 07_INHERITANCE_ADHOCPOLYMORPHISM/Phones/OldPhone.java
@@ -1,10 +1,10 @@
 //cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/Phones
-// Exercise 10.1. Introducing ad hoc polymorphism (aka inheritance) and casting.
+// Exercise 10. Introducing ad hoc polymorphism (aka inheritance) and casting.
 /*
+10.1:
 Create a class OldPhone that implements the following interface.
 
 public interface Phone {
-
   javadoc: Just print on the screen: "Calling <number>...".
   void call(String number);
 }
@@ -27,10 +27,57 @@ public class PhoneLauncher {
   public void launch() {
     // your code creating and using SmartPhone here...
   }
-}*/
+}
+
+10.3:
+Add the following field, constructor, and method to OldPhone:
+private String brand = null;
+public OldPhone(String brand) {
+  this.brand = brand;
+}
+public String getBrand() {
+  return brand;
+}
+// ... there is no setter for brand
+Add the appropriate constructors to MobilePhone and SmartPhone in order
+to be able to call the method getBrand() from an object of class SmartPhone
+and obtain the right answer, i.e. the brand provided in the constructor.
+*/
+
+import interfacePhone.Phone;
 
 public class OldPhone implements Phone {
 
+  private String brand = null;
+
+  /**
+  * Constructor.
+  * @param brand brandname string of the phone.
+  */
+  public OldPhone(String brand) {
+
+    this.brand = brand;
+
+  }
+
+  /**
+  * Zero-arg constructor
+  */
+  public OldPhone(){}
+
+  /**
+  * @returns phone's brandname string.
+  */
+  public String getBrand() {
+
+    return brand;
+
+  }
+
+  /**
+  * Calls phone numbers.
+  * @param number the number string being called.
+  */
   @Override
   public void call(String number) {
 
