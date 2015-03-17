cef2f1971715eb30b391071a6e9edda0a311d945
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-03-17 16:14:07 +0000

    removed all NoahsArk using Atom

--- 07_INHERITANCE_ADHOCPOLYMORPHISM/NoahsArk/Aves.java
+++
@@ -1,8 +0,0 @@
-//cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/07_INHERITANCE_ADHOCPOLYMORPHISM/Phones
-// Exercise 10.8: Introducing ad hoc polymorphism (aka inheritance).
-
-public abstract class Aves implements Vertebrata {
-
-
-
-}
