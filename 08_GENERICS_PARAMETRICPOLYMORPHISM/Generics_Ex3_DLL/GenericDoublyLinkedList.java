9d8cb4c1518bf14c889893d22ac206dd8bbfb380
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-04-12 16:28:02 +0100

    minor edit

--- 08_GENERICS_PARAMETRICPOLYMORPHISM/Generics_Ex3_DLL/GenericDoublyLinkedList.java
+++ 08_GENERICS_PARAMETRICPOLYMORPHISM/Generics_Ex3_DLL/GenericDoublyLinkedList.java
@@ -1,10 +1,11 @@
-// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/08_GENERICS_PARAMETRICPOLYMORPHISM/Generics_Ex3
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/08_GENERICS_PARAMETRICPOLYMORPHISM/Generics_Ex3_DLL
 // Exercise 11.3: Intro to parametric polymorphism (aka Generics).
 /*
 Modify the doubly-linked list that you have created in past weeks to make it generic,
 i.e. to allow it to have values of its elements of any type.
 */
 
+package Generics_Ex3_DLL;
 //This is the Doubly-linked list (from Ex 7.5), AFTER editting to generic format.
 public class GenericDoublyLinkedList<T> {
 
