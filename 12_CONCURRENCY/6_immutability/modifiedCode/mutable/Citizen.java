3e896eb8a502f89a4a325ecbded02fdf967b3e5c
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-05-01 21:32:49 +0100

    same as teacherCode but with defensive copies

---
+++ 12_CONCURRENCY/6_immutability/modifiedCode/mutable/Citizen.java
@@ -0,0 +1,43 @@
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/12_CONCURRENCY/6_immutability/modifiedCode/mutable
+// Exercise 17.6: Introduction to concurrency
+/*
+I think Citizen & Officer can be refered to as 'clients' of the mutable class
+(MutableIDCard), so according to rule#5 (see readme), I should make sure that
+neither can obtain references to the mutable fields of MutableIDCard (which
+include Date and BufferedImage). Hence I am creating a 'defensive copy' of
+MutableIDCard in the constructor. (Is this correct?)
+*/
+
+public class Citizen implements Runnable {
+
+  private MutableIDCard id;
+
+  public Citizen(MutableIDCard id) {
+
+    this.id = new MutableIDCard(id);// making a defensive copy
+
+  }
+
+  public void run() {
+
+    Random r = new Random();
+
+    try {
+
+      Thread.sleep(r.nextInt(2000));
+
+    } catch (InterruptedException e) {
+
+      e.printStackTrace();
+
+    }
+
+    String message = "";
+    message += "the name on this id is " + id.getName() + "\n";
+    message += "and the date of birth is " + id.getDateOfBirth();
+
+    System.out.println(message);
+
+  }
+
+}
---
+++ 12_CONCURRENCY/6_immutability/modifiedCode/mutable/Officer.java
@@ -0,0 +1,42 @@
+// cd ~/Desktop/ALL_POOLED/COMPUTING/CODING/JavaExercises/12_CONCURRENCY/6_immutability/modifiedCode/mutable
+// Exercise 17.6: Introduction to concurrency
+/*
+I think Citizen & Officer can be refered to as 'clients' of the mutable class
+(MutableIDCard), so according to rule#5 (see readme), I should make sure that
+neither can obtain references to the mutable fields of MutableIDCard (which
+include Date and BufferedImage). Hence I am creating a 'defensive copy' of
+MutableIDCard in the constructor. (Is this correct?)
+*/
+
+public class Officer implements Runnable {
+
+  private MutableIDCard id;
+
+  public Officer(MutableIDCard id) {
+
+    this.id = new MutableIDCard(id);// making a defensive copy
+
+  }
+
+  public void run() {
+
+    Random r = new Random();
+
+    try {
+
+      Thread.sleep(r.nextInt(2000));
+
+    } catch (InterruptedException e) {
+
+      e.printStackTrace();
+
+    }
+
+    String name = DataGenerator.getNextName();
+    Date dateOfBirth = DataGenerator.getNextDate();
+    BufferedImage photo = DataGenerator.getNextPhoto();
+    id.set(name, dateOfBirth, photo);
+
+  }
+
+}
